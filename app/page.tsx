2cb32ff0978db4391612a5e26f888090874c0bb4
AnikaTahshin <anikatahsinmeem@gmail.com>
2024-02-05 16:15:51 +0600

    front page design

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col md:flex-row items-center justify-around p-24">
     {/* left side starts */}
     
-     <div className="w-[500px]">
-      <p className="text-[12px]">100% QUALITY, 100% SATISFACTION</p>
-      <p className="text-[64px]">Where the world comes to shop.</p>
-      <p className="text-[18px]">Life is hard enough already. Let us make it a little easier, Vision of Snipshop for a better outlook.</p>
+     <div className="w-full sm:w-[250px] md:w-[500px]">
+      <p className="text-[6px] md:text-[12px]">100% QUALITY, 100% SATISFACTION</p>
+      <p className="text-[32px] md:text-[64px]">Where the world comes to shop.</p>
+      <p className="text-[9px] md:text-[18px]">Life is hard enough already. Let us make it a little easier, Vision of Snipshop for a better outlook.</p>
 
-      <div className="flex flex-row justify-between items-center">
-      <div className="bg-black rounded-full p-2 w-[145px] flex flex-col justify-center items-center">
-        <button className="text-white ">Shop Now</button>
+      <div className="flex flex-col md:flex-row justify-between items-center">
+      <div className="bg-black rounded-full p-1 md:p-2 w-[120px] md:w-[145px] flex flex-col justify-center items-center">
+        <button className="text-white text-[12px] md:text-[16px]">Shop Now</button>
         
       </div>
       <div >
-          <p>Start from</p>
-          <p>110.00$</p>
+          <p className="text-[8px] md:text-[16px]">Start from</p>
+          <p className="text-[12px] md:text-[16px]">110.00$</p>
         </div>
       </div>
      </div>
@@ -25,15 +25,15 @@ export default function Home() {
      
 
      {/* middle side starts */}
-     <div className="rounded-full p-4  bg-white w-[400px] h-[400px] flex justify-center items-center">
+     <div className="rounded-full p-4 border-solid border-cyan-500 border-[8px] bg-white w-[280px]  h-[280px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] flex justify-center items-center">
       
       <img className="object-contain" src="../assets/logo.png" alt="" />
       
      </div>
      {/* middle side starts */}
 
-     <div className="flex flex-col justify-center items-center">
-     <div className="rounded-full p-4 border-solid border-cyan-500 bg-white w-[130px] h-[130px] flex justify-center items-center mb-4">
+     <div className="flex flex-col justify-center items-center ">
+     <div className="rounded-full p-4  bg-white w-[130px] h-[130px] flex justify-center items-center mb-4">
       
       <img className="object-contain" src="../assets/logo.png" alt="" />
       
